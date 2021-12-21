380a7cf04623ab945acdaf90e253c87014479707
Mike Wazovzky <mike.wazovzky@gmail.com>
2021-12-21 16:45:57 +0300

    Show exhange rate

---
+++ src/components/ShowRate.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  rate: Object,
+});
+</script>
+
+<template>
+  <h1>Exchange Rate</h1>
+  <div class="currency">{{ rate.Currency }}</div>
+  <div class="value">{{ rate.Value }}</div>
+</template>
+
+<style scoped>
+.currency {
+  color: #42b983;
+  font-weight: bold;
+  font-size: 3em;
+}
+.value {
+  font-size: 5em;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,15 @@
 import { computed, ref } from 'vue'
 import SelectCurrency from '@/components/SelectCurrency.vue'
 import SelectDate from '@/components/SelectDate.vue'
+import ShowRate from '@/components/ShowRate.vue'
 
 const loading = ref(true)
 const rates = ref([])
 const currency = ref("USD")
 const date = ref(new Date())
+
 const currencies = computed(() => rates.value.map(r => r.Currency))
+const rate = computed(() => rates.value.find(r => r.Currency == currency.value))
 
 fetch(`http://localhost:8080/api/rates`)
   .then(r => r.json())
@@ -20,4 +23,5 @@ fetch(`http://localhost:8080/api/rates`)
 <template>
   <SelectCurrency v-if="!loading" :currencies="currencies" v-model:currency="currency"/>
   <SelectDate v-model:date="date"/>
+  <ShowRate v-if="!loading" :rate="rate"/>
 </template>
